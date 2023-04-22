ff393c57ec7b70e224f320489b0e8efb7ad00ded
cesar ades <cesaradese@gmail.com>
2023-04-21 21:17:11 -0500

    Added multi threading to the scraper to make it faster

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from prop_scraper import *
 
-players = scrape(
-    "https://www.bettingpros.com/nba/odds/player-props/?date=2023-04-18",
-    "?date=2023-04-18",
-    testing=True)
+players = scrape("https://www.bettingpros.com/nba/odds/player-props/",
+                 "?date=2023-04-22",
+                 display=True)
 
-print(players[0])
-print(player[0].props[0])
+for player in players:
+    print(player)
--- prop_scraper.py
+++ prop_scraper.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import concurrent.futures
 
 
 class Player:
@@ -13,6 +16,10 @@ class Player:
         self.props = props if props is not None else []
 
     def __str__(self):
+        border = "*" * (len(self.name) + 6)
+        return f"{border}\n** {self.name} **\n{border}"
+
+    def __repr__(self):
         border = "*" * (len(self.name) + 6)
         return f"{border}\n** {self.name} **\n{border}"
 
@@ -36,70 +43,84 @@ class Prop:
         return f"{self.name}:\t{self.over_line} {self.over_cost}\t{self.under_line} {self.under_cost}"
 
 
-def scrape(url, date, testing=False):
+def scrape_player_props(player_href, display=False):
 
-    # list of the players and their props
-    players = []
+    # Create a new player object
+    name = " ".join(n.capitalize()
+                    for n in player_href.split("/")[-2].split("-"))
+    if display: print(f"Scraping {name}")
+    player = Player(name)
 
-    # Configure Chrome options
+    # Configure Chrome options and create instance
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-
-    # Create a new browser instance
-    # browser = webdriver.Safari(options=safari_options)
     browser = webdriver.Chrome(options=chrome_options)
 
-    # Navigate to the URL
-    browser.get(url)
+    # Navigate to the URL and get the props
+    browser.get(player_href)
+    wait = WebDriverWait(browser, 20)
+    props = wait.until(
+        EC.presence_of_all_elements_located(
+            (By.CLASS_NAME, "grouped-items-with-sticky-footer__content")))
 
-    # Timeout after 10 seconds
-    wait = WebDriverWait(browser, 10)
+    # Loop through the props and get the consensus
+    for prop in props:
+        try:
+            # Get the name of the prop
+            pname = prop.find_element(By.TAG_NAME, "span").text
+            if "Over/Under" not in pname:
+                continue
+
+            # Get the consensus lines and costs
+            con = prop.find_elements(By.CLASS_NAME, "odds-offer__item")[-1]
+            lines = [
+                l.text
+                for l in con.find_elements(By.CLASS_NAME, "odds-cell__line")
+            ]
+            costs = [
+                c.text
+                for c in con.find_elements(By.CLASS_NAME, "odds-cell__cost")
+            ]
+
+            # Add the prop to the player
+            player.add_prop(
+                Prop(pname, (lines[0], costs[0]), (lines[1], costs[1])))
+        except:
+            continue
+
+    # Close the browser instance
+    browser.quit()
+
+    if display: print(f"Finished scraping {name}.")
+    return player
+
+
+def scrape(url, date, display=False):
+
+    # Configure Chrome options and create instance
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    browser = webdriver.Chrome(options=chrome_options)
+
+    # Navigate to the URL and get the player cards
+    browser.get(url + date)
+    wait = WebDriverWait(browser, timeout=10)
     player_cards = wait.until(
         EC.presence_of_all_elements_located((By.CLASS_NAME, "player-card")))
 
-    # get hrefs for each player
+    # Get hrefs for each player
     hrefs = [
-        f"{p.find_element(By.TAG_NAME, 'a').get_attribute('href')}{date}"
+        p.find_element(By.TAG_NAME, 'a').get_attribute('href')
         for p in player_cards
     ]
 
-    # loop through hrefs and get prop data
-    for href in hrefs:
-
-        # get player name
-        name = href.split("/")[-2].split("-")
-        name = " ".join([n.capitalize() for n in name])
-
-        # create a new player object
-        player = Player(name)
-
-        # wait for the page to load
-        browser.get(href)
-        wait = WebDriverWait(browser, 10)
-        player_props = wait.until(
-            EC.presence_of_all_elements_located(
-                (By.CLASS_NAME, "grouped-items-with-sticky-footer__content")))
-
-        for player_prop in player_props:
-            name = player_prop.find_element(By.TAG_NAME, "span").text
-            if "Over/Under" not in name:
-                continue
-            consensus = player_prop.find_elements(By.CLASS_NAME,
-                                                  "odds-offer__item")[-1]
-            lines = [
-                l.text for l in consensus.find_elements(
-                    By.CLASS_NAME, "odds-cell__line")
-            ]
-            costs = [
-                c.text for c in consensus.find_elements(
-                    By.CLASS_NAME, "odds-cell__cost")
-            ]
-            player.add_prop(
-                Prop(name, (lines[0], costs[0]), (lines[1], costs[1])))
-        if testing:
-            print("\n\n", player)
-            for prop in player.props:
-                print(prop)
+    # Use concurrent.futures to scrape each player's props
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        players = list(
+            executor.map(
+                lambda href: scrape_player_props(href + date, display=display),
+                hrefs))
+        executor.shutdown(wait=True)
 
     # Close the browser instance
     browser.quit()
