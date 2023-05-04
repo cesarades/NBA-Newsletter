394e35235e652ffbaaf4a4fdb19b910021784933
vatsal.bhargava <vatsal.bhargava@us.kbr.com>
2023-05-03 21:43:36 -0500

    Going back version of main.;y

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from scraper import *
 from nba_data import *
 from player import *
 from csv_reader import *
@@ -12,8 +11,8 @@ from datetime import time
 player_stats = get_player_stats("2023-04-29")
 bets = csv_to_dict()
 player_props = scrape("https://www.bettingpros.com/nba/odds/player-props/",
-                      "?date=2023-04-29",
-                      display=True)
+                 "?date=2023-04-29",
+                 display=True)
 
 # Have all info we need, now feed information from this into prompt
 # Get the value of all stats from day's game of players whose bets were taken
@@ -22,13 +21,7 @@ bets_results = {}
 for player in bets:
     # order of values being placed in there are points, rebounds, assists, FGM, FGA, time played
     curr_row = player_stats[player_stats['PLAYER_NAME'] == player]
-    bets_results[player] = [
-        int(curr_row['PTS'].tolist()[0]),
-        int(curr_row['REB'].tolist()[0]),
-        int(curr_row['AST'].tolist()[0]),
-        int(curr_row['FGM'].tolist()[0]),
-        int(curr_row['FGA'].tolist()[0]), (curr_row['MIN'].tolist()[0])
-    ]
+    bets_results[player] = [int(curr_row['PTS'].tolist()[0]), int(curr_row['REB'].tolist()[0]), int(curr_row['AST'].tolist()[0]), int(curr_row['FGM'].tolist()[0]), int(curr_row['FGA'].tolist()[0]), (curr_row['MIN'].tolist()[0])]
 
 # dictionary of averages of players who got bet, order = points, rebounds, assists, fgm, fga, time_played
 season_averages = {}
@@ -41,10 +34,9 @@ for player in bets:
     city = bets[player][0][0]
     if city not in teams:
         teams.append(city)
-
+        
 # Convert 'MIN' column to a numeric type (float) and handle any errors
-player_stats['MIN'] = pd.to_datetime(player_stats['MIN'],
-                                     format='%M:%S').dt.time
+player_stats['MIN'] = pd.to_datetime(player_stats['MIN'], format='%M:%S').dt.time
 
 # dictionary of cities to list of players and their stats
 teammate_stats = {}
@@ -52,14 +44,12 @@ for city in teams:
     if city not in teammate_stats:
         # select rows where the 'TEAM_CITY' column is 'Phoenix' and the 'MIN' column is greater than 5 minutes
         # df_filtered = player_stats[(player_stats['MIN'] != '00:00')]
-        df_filtered = player_stats[(player_stats['TEAM_CITY'] == city) & (
-            player_stats['MIN'] > pd.to_datetime('00:20:00').time())]
+        df_filtered = player_stats[(player_stats['TEAM_CITY'] == city) & (player_stats['MIN'] > pd.to_datetime('00:20:00').time())]
         # Loop over rows and put into the dictionary
         teammate_stats[city] = {}
         for index, row in df_filtered.iterrows():
-            teammate_stats[city][row['PLAYER_NAME']] = [
-                row['PTS'], row['REB'], row['AST'], row['FGM'], row['FGA']
-            ]
+            teammate_stats[city][row['PLAYER_NAME']] = [row['PTS'], row['REB'], row['AST'], row['FGM'], row['FGA']]
+            
 
 prompt = """
 Ignore all previous instructions. I am going to give you context of what you are going to do. I will give you specific stats of NBA players and their predicted stats from before the game. Given certain criteria, you will infer why one player did better or worse than predicted lines based upon the criteria I give you.
@@ -88,7 +78,7 @@ for player_object in player_props:
     #player we care about, go into their props and fill out bet_lines dictionary
     for prop in player_object.props:
         bet_lines[player_object.name][prop.name] = prop.over_line
-
+    
 # Now generate for each bet the required info
 
 bets_sentence = "I bet on "
@@ -106,10 +96,8 @@ for player in bets:
     assists = bets_results[player][2]
     fgm = bets_results[player][3]
     fga = bets_results[player][4]
-    shooting_perc = round(fgm / fga, 2) * 100
-    prompt += player + " had " + str(points) + " points, " + str(
-        rebounds) + " rebounds, and " + str(assists) + " assists on " + str(
-            shooting_perc) + "% shooting.\n"
+    shooting_perc = round(fgm/fga,2) * 100 
+    prompt += player + " had " + str(points) + " points, " + str(rebounds) + " rebounds, and "+ str(assists) + " assists on " + str(shooting_perc)+ "% shooting.\n"
 
 for player in bets:
     # Need player's city
@@ -119,21 +107,15 @@ for player in bets:
     for mate in teammates:
         if mate == player:
             continue
-        prompt += (mate + ": " + str(teammates[mate][0]) + " points, " +
-                   str(teammates[mate][1]) + " assists, and " +
-                   str(teammates[mate][2]) + " rebounds on " +
-                   str(teammates[mate][-2]) + " field goals made of " +
-                   str(teammates[mate][-1]) + " field goals attempted.\n\n")
-
+        prompt += (mate + ": " + str(teammates[mate][0]) + " points, " + str(teammates[mate][1]) + " assists, and " + str(teammates[mate][2]) + " rebounds on " + str(teammates[mate][-2]) + " field goals made of " + str(teammates[mate][-1]) + " field goals attempted.\n\n")
+    
     # Add players averages below this each time
     p_avg = season_averages[player]
-    prompt += (player + " averaged " + str(p_avg[0]) + " points, " +
-               str(p_avg[1]) + " rebounds, and " + str(p_avg[2]) +
-               " assists so far this season.")
+    prompt += (player + " averaged " + str(p_avg[0]) + " points, " + str(p_avg[1]) + " rebounds, and " + str(p_avg[2]) + " assists so far this season.")
 
-prompt += """\nMake sure you do not say anything that is too big of a logical leap,
-use the other teammates data to infer things about why things happened for my bets. We
-want to understand based on the numbers why my bets hit or missed. Use their season averages to
+prompt += """\nMake sure you do not say anything that is too big of a logical leap, 
+use the other teammates data to infer things about why things happened for my bets. We 
+want to understand based on the numbers why my bets hit or missed. Use their season averages to 
 give a full picture of what happened
 
 """
